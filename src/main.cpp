49d3c640774b21dd5938c1fa0592d3180618bb62
FeudMe <56523984+FeudMe@users.noreply.github.com>
2023-06-15 13:06:59 +0200

    update main.cpp to contain meaningful example

--- src/main.cpp
+++ src/main.cpp
@@ -5,11 +5,23 @@
 int main() {
     adj_set_graph g = adj_set_graph();
 
-    g.add_node();
-    g.add_node();
+    g.add_node(); // 0
+    g.add_node(); // 1
+    g.add_node(); // 2
+    g.add_node(); // 3
+    g.add_node(); // 4
+
     g.add_edge(0, 1, 0.5);
+    g.add_edge(0, 2, 0.2);
+    g.add_edge(0, 3, 0.4);
+    g.add_edge(3, 2, 0.6);
 
     g.print_graph();
 
+    g.remove_node(0);
+
+
+    std::cout << std::endl;
+    g.print_graph();
     return 0;
 }
